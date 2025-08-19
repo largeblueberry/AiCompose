5aac4789d4f475068b875f820e4abc89475d2deb
largeblueberry <onlym3480@naver.com>
2025-08-19 19:07:56 +0900

    [fix] : 네트워크 및 초기 설정 오류 수정 1. Retrofit의 baseUrl이 BuildConfig.BASE_URL을 올바르게 참조하도록 수정하여 IllegalArgumentException 해결 2.특정 액티비티에서 누락된 어노테이션을 추가하여 초기 설정 오류 방지

--- app/src/main/java/com/largeblueberry/aicompose/record/ui/RecordActivity.kt
+++ app/src/main/java/com/largeblueberry/aicompose/record/ui/RecordActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.largeblueberry.aicompose.record.ui.screen.RecordScreenState
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class RecordActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
